50f06d63b7c9366d6d9cb4aba86627bf11c32ccf
Alexander Berntsson <alexande.berntsson@gmail.com>
2024-02-25 12:33:16 +0100

    fix nix-shell enviornment for building

--- shell.nix
+++ shell.nix
@@ -1,8 +1,17 @@
 let 
-  pkgs = import <nixpkgs> {};
+  rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> {
+    overlays = [
+      rust-overlay
+    ];
+  };
 in 
-pkgs.mkShell {
+pkgs.mkShell rec {
   packages = with pkgs; [
+    elf2uf2-rs
+    (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+  ];
+  buildInputs = with pkgs; [
     eudev
     pkg-config
   ];
